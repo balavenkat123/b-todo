3dd625a1469db58272af5b0496bb0eae4d392c04
bala venkat <balavenkat17902@gmail.com>
2023-08-10 11:00:18 -0700

    mongodb connected

--- backend/Server.js
+++ backend/Server.js
@@ -6,4 +6,9 @@ const app= express()
 
 const PORT =process.env.port || 5000
 
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(() => console.log(`connecting to mongodb....`))
+.catch((err)=> console.log(err))
+
 app.listen(PORT, () => console.log(`listen onnnn :${PORT}`))
