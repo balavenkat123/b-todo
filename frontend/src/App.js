3b7ef381395bf7ae8cd0004220bc01f1c02bb525
bala venkat <balavenkat17902@gmail.com>
2023-08-15 04:04:39 -0700

    initiallizing react for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="container">
+        <h1>To Do Tasks...</h1>
+        <div className="top" >
+          <input type="text" placeholder="enter task" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
